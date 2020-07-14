4d147a499b8da30f4398d98d8d3d2b07d6f5a0d3
Damian Cisneros <dcisneros@poli.policlinicomodelo.com.ar>
2020-07-14 10:28:55 -0300

    Se separo pantalla de alta de categoria

--- src/app/components/acategoria/acategoria.component.ts
+++ src/app/components/acategoria/acategoria.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ICategoria } from 'src/app/models/categoria.interface';
+import { ProvidersService } from 'src/app/services/providers.service';
 
 @Component({
   selector: 'app-acategoria',
@@ -7,9 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ACategoriaComponent implements OnInit {
 
-  constructor() { }
+  categorias: ICategoria[];
+  categoriaSeleccionada: ICategoria = {
+    cat_id: '', cat_descripcion: ''
+  };
+
+  soloLectura = true;
+
+  constructor(private provS: ProvidersService) {
+    this.categorias = this.provS.getCategorias();
+   }
 
   ngOnInit() {
+  }
+
+  guardar() {
+    console.log(this.categoriaSeleccionada);
+  }
+
+  modificar() {
+    this.soloLectura = false;
+  }
+  agregar() {
+    this.soloLectura = false;
+    this.categoriaSeleccionada = {
+      cat_id: '', cat_descripcion: ''
+    };
+  }
+
+  esNuevo() {
+    return (this.categoriaSeleccionada === {cat_id: '' , cat_descripcion: ''});
+  }
+
+  seleccionar(categoria: string) {
+    this.categoriaSeleccionada = this.provS.getCategoria(categoria)[0];
+    this.soloLectura = true;
   }
 
 }
--- src/app/components/altas/altas.component.ts
+++ src/app/components/altas/altas.component.ts
@@ -13,42 +13,7 @@ export class AltasComponent implements OnInit {
   esEquipo = false;
   esJugador = false;
 
-  categorias: ICategoria[];
-  categoriaSeleccionada: ICategoria = {
-    cat_id: '', cat_descripcion: ''
-  };
-
-  soloLectura = true;
-  constructor(private provS: ProvidersService) {
-    this.categorias = this.provS.getCategorias();
-   }
 
   ngOnInit() {
   }
-
-  seleccionaOpcion() {
-  }
-  guardar() {
-    console.log(this.categoriaSeleccionada);
-  }
-
-  modificar() {
-    this.soloLectura = false;
-  }
-  agregar() {
-    this.soloLectura = false;
-    this.categoriaSeleccionada = {
-      cat_id: '', cat_descripcion: ''
-    };
-  }
-
-  esNuevo() {
-    return (this.categoriaSeleccionada === {cat_id: '' , cat_descripcion: ''});
-  }
-
-  seleccionar(categoria: string) {
-    this.categoriaSeleccionada = this.provS.getCategoria(categoria)[0];
-    this.soloLectura = true;
-  }
-
 }
